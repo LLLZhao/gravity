51f60c400403ebcae4d08cbeac98ec08054a58ff
LLLZhao <1005570863@qq.com>
2017-04-04 14:28:00 -0300

    Get the value through the method

--- src/main/java/dal/gravity/RegularPendulum.java
+++ src/main/java/dal/gravity/RegularPendulum.java
@@ -1,36 +1,38 @@
 package dal.gravity;
 
 /**
+ * 
  * Represents a pendulum
  */
 public class RegularPendulum extends AbstractEarthPendulum {
+    private double planet = gravity; 
     private double delta, iterations = 0;
     private double dissipation;
     private double lastTheta, lastVel, lastAccel;
+    
 
     /**
      * Creates a new Pendulum instance 
      */
-    public RegularPendulum (double inLength, double inMass, double inTheta0, 
-		     double inDelta, double inDiss) {
+    public RegularPendulum (double inLength, double inMass, double inTheta0, double inDelta, double inDiss) {
 	super (inLength, inMass, inTheta0);
 	delta=inDelta;
 	dissipation = inDiss;
 	lastVel = 0;
 	lastTheta = this.getMaxAngularDisplacement ();
-	lastAccel = -(this.getGravitationalField () / this.getStringLength ())*Math.sin (lastTheta);
+	lastAccel = -(grav/ this.getStringLength ())*Math.sin (lastTheta);
+	gravityInter.GravityConstant(planet);
     }
 
-    public RegularPendulum (double inLength, double inMass, double inTheta0, 
-		     double inDelta) {
-	this (inLength, inMass, inTheta0, inDelta, 0);
+    public RegularPendulum (double inLength, double inMass, double inTheta0, double inDelta) {
+    	this (inLength, inMass, inTheta0, inDelta, 0);
     }
 
     public void step () {
 	iterations++;
 	lastTheta = lastTheta + lastVel*delta;
 	lastVel = lastVel + lastAccel*delta;
-	lastAccel = - dissipation*lastVel - this.getGravitationalField () / this.getStringLength () * Math.sin (lastTheta);
+	lastAccel = - dissipation*lastVel - grav/ this.getStringLength () * Math.sin (lastTheta);
     }
 
     public double getLastTheta () { return lastTheta; }
